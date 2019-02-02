b276653798b039effdf5ca37ad9f383b810c2f4d
Thomas Nilefalk <thomas@junovagen.se>
2019-02-03 00:59:56 +0100

    Fixup cpp test for lmk

--- src/lmk/testing/execute.bat
+++
@@ -1,8 +0,0 @@
-rem Execute test cases on the PC
-call mkwin32 ansi
-call b59.bat
-call mkwin32 cpp
-call mkwin32 imp
-call mkwin32 lmk
-call mkwin32 pmk
-call mkwin32 smk
--- src/lmk/testing/mkwin32.bat
+++
@@ -1,6 +0,0 @@
-@rem  Run lmk on .lmk file named by first argument 
-@rem  Make .tst.win32 file containing stderr output followed by lmk output. 
-@..\lmk %1 > %1.tst.tmp 
-@move /Y stderr.txt %1.tst.win32 
-@type %1.tst.tmp >> %1.tst.win32 
-@del %1.tst.tmp 
