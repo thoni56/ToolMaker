8f14aa63553992770128578b16820b13c671e135
Thomas Nilefalk <thomas@junovagen.se>
2019-02-03 00:49:55 +0100

    Fixed two tests for lmk

--- src/lmk/testing/b59.bat
+++
@@ -1,8 +0,0 @@
-..\lmk -lib .. b59.lmk > b59.tst.tmp 
-move /Y stderr.txt b59.tst.win32 
-type b59.tst.tmp >> b59.tst.win32 
-cl -DWIN32 -o b59 b59List.c b59.c 
-b59.exe -list unknown\unknown b59.c >> b59.tst.tmp 
-type stderr.txt >> b59.tst.win32 
-type b59.tst.tmp >> b59.tst.win32 
-del b59.tst.tmp 
