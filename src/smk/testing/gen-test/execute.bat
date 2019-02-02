ed3913ad0eaec92e9e87a9e1d8320a3b59f3284d
Thomas Nilefalk <thomas@junovagen.se>
2019-02-02 18:27:36 +0100

    Clean up all gen-tests for smk

--- src/smk/testing/gen-test/execute.bat
+++
@@ -1,7 +0,0 @@
-rem Execute test cases on the PC
-call mkwin32 imp 
-call mkwin32 lmk 
-call mkwin32 pmk 
-call mkwin32 smk 
-call mkwin32 tmcore 
-call mkwin32 tmk 
--- src/smk/testing/gen-test/tmcore.tst
+++
@@ -1,7 +0,0 @@
-set os = "$argv[1]"
-if ("$os" == "PC") then
-  dos2unix -ascii tmcore.tst.win32
-  alias cat dos2unix -ascii
-else
-  ../../smk tmcore
-endif
