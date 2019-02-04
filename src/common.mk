0735cb5898648f15149b1bd890e37692cdf2f72c
Thomas Nilefalk <thomas@junovagen.se>
2019-02-04 21:23:04 +0100

    Handle TMARCH as a macro

--- src/common.mk
+++ src/common.mk
@@ -1,5 +1,8 @@
 #Some common targets for all ToolMaker components
 
+# TODO: generate 'cygwin32' or 'cygwin64' and handle that in all skeleton files to
+TMARCH = ${if ${findstring CYGWIN, ${shell uname}},Cygwin,${strip ${shell uname}}}
+
 checkARCH:
 	@if test -f .arch ; then :; else echo "none" > .arch; fi
 	@-if test "`cat .arch`" != "$(TMARCH)"; then \
