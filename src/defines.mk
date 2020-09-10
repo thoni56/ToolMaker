e149e018fc18a49d491188a055e17b1fe16354a9
Thomas Nilefalk <thomas@junovagen.se>
2020-09-10 11:39:37 +0200

    Use cygwin32 instead of cygwin

--- src/defines.mk
+++ src/defines.mk
@@ -1,8 +1,7 @@
 # Common defines for ToolMaker build system
 
-# TODO: generate 'cygwin32' or 'cygwin64' and handle that in all skeleton files too
 UNAME = ${shell uname}
-TMARCH := ${if ${findstring CYGWIN,$(UNAME)},${if ${findstring WOW, $(UNAME)},cygwin32,cygwin64},${strip ${shell uname}}}
+TMARCH := ${if ${findstring CYGWIN,$(UNAME)},${if ${findstring WOW, $(UNAME)},cygwin32,cygwin64},${strip ${UNAME}}}
 
 DESTROOT=/usr/local
 DESTLIB=$(DESTROOT)/lib/ToolMaker
