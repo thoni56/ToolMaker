2b0ecb2d24efcc8696e2bc656733fa04c0fe77f1
Thomas Nilefalk <thomas@nilefalk.se>
2020-09-10 11:09:00 +0200

    Add filtering for some OS variables in pmk tests

--- src/smk/testing/gen-test/mkwin32.bat
+++
@@ -1,6 +0,0 @@
-@rem  Run smk on .smk file named by first argument 
-@rem  Make .tst.win32 file containing stderr output followed by smk output. 
-@..\..\smk -generate tables -generate source %1 > %1.tst.tmp 
-@move /Y stderr.txt %1.tst.win32 
-@type %1.tst.tmp >> %1.tst.win32 
-@del %1.tst.tmp 
