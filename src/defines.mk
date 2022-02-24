5017627636d20083dd788335089e7f46753adf09
Thomas Nilefalk <thomas@nilefalk.se>
2022-02-24 11:42:38 +0100

    Remove instructions for cross-compilation on cygwin64, now that it works!

--- src/defines.mk
+++ src/defines.mk
@@ -26,16 +26,9 @@ endif
 DESTROOT=/usr/local
 DESTLIB=$(DESTROOT)/lib/ToolMaker
 
-ifeq ($(TMARCH),cygwin64)
-$(info INFO: Cross-compiling to Cygwin32)
-$(info INFO: You need the packages 'cygwin32-gcc-core' (for cross-compiler) and 'cygwin32-w32api-runtime' (for runtime))
-$(info INFO: You also need to 'export PATH=/usr/i686-pc-cygwin/sys-root/usr/bin/:$$PATH' to run the binaries)
-CC = i686-pc-cygwin-gcc
-CFLAGS = -MMD -Wall -g $(INCLUDE) -DTARGETOS=\"$(TMARCH)\"
-else
 ifeq ($(TMARCH),mingw64)
 $(error MINGW64 not supported, build on MINGW32)
 endif
 CC = gcc
-CFLAGS = -MMD -Wall -g $(INCLUDE) -DTARGETOS=\"$(TMARCH)\" $(BITNESS)
-endif
+CFLAGS = -MMD -Wall -ggdb $(INCLUDE) -DTARGETOS=\"$(TMARCH)\" $(BITNESS)
+
